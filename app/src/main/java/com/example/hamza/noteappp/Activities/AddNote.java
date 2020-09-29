a00434e3850d93d3d90a661754a7164d671e3ad3
hamzagamal <54688005+hamza94max@users.noreply.github.com>
2020-09-30 00:15:58 +0200

    addNote

---
+++ app/src/main/java/com/example/hamza/noteappp/Activities/AddNote.java
@@ -0,0 +1,54 @@
+package com.example.hamza.noteappp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hamza.noteappp.R;
+
+public class AddNote extends AppCompatActivity {
+    public static final String EXTRA_TITLE ="TITLE";
+    public static final String EXTRA_DESCRIPTION ="CONTENT";
+
+
+    private EditText title;
+    private EditText content;
+    private Button add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        title=findViewById(R.id.title);
+        content=findViewById(R.id.contentt);
+
+        add =findViewById(R.id.add_btn);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (title.getText().toString().trim().isEmpty() || content.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please insert a title and description", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent data = new Intent();
+                data.putExtra(EXTRA_TITLE, title.getText().toString());
+                data.putExtra(EXTRA_DESCRIPTION, content.getText().toString());
+
+                setResult(RESULT_OK, data);
+                Toast.makeText(getApplicationContext(), "Note saved", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+
+
+
+
+    }
+}
